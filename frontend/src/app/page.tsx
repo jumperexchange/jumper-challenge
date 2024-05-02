15210dd1cb3b416b4f12ce404abb057405ffe0ca
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-05-02 11:37:34 +0700

    Add default frontend setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Box display="flex" justifyContent="center">
+      <Typography variant="h1">Welcome to Jumper challenge!</Typography>
+    </Box>
+  );
+}
